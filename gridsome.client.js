4bd83231bc945d7f1a2937438d68ed18bf043e39
Frank Buncom IV <84871672+fbuncom4@users.noreply.github.com>
2021-09-25 12:10:22 -0400

    Flip console error messages

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,10 +1,10 @@
 export default function (Vue, options, context) {
-    if (!options.dataWebsiteId) {
-        console.error('Please supply dataWebsiteId in options for gridsome-plugin-umami.')
-        return;
-    }
     if (!options.srcUrl) {
         console.error('Please supply srcUrl in options for gridsome-plugin-umami.')
+        return;
+    }
+    if (!options.dataWebsiteId) {
+        console.error('Please supply dataWebsiteId in options for gridsome-plugin-umami.')
         return;
     }
 
